7522e27bb69ef97a3990e68f06c5ce61471a4e97
Tony Wu <tony@computhought.com.au>
2021-10-25 00:12:51 +1100

    Updated e2e test.

--- salesperson-web/e2e/src/app.e2e-spec.ts
+++ salesperson-web/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  it('should display title', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('salesperson-web app is running!');
+    expect(await page.getTitleText()).toEqual('Sales Person Web');
   });
 
   afterEach(async () => {
